70bbd6795d9ac9862c3eadc56c42bda75b248004
Igor <mr.ryaboff2013@yandex.ru>
2022-04-26 19:35:25 +0600

    Probing every 15 seconds; Port check; Web monitoring UI

--- app.js
+++ app.js
@@ -2,26 +2,42 @@ const axios = require('axios').default;
 try { require('./etc/config.json'); } catch { console.log('No config file :('); process.exit(255); }
 const config = require('./etc/config.json');
 const probe = require('./probe');
+const Express = require('express');
+const ExpressBasicAuth = require('express-basic-auth');
 
+/** @type {Object<string, Monitor>} */
 let monitors = {};
+/** @type {Object<string, string>} */
+let lastMonitorStatus = {};
 
-async function cycle() {
-    let requestData = {};
-    let promises = [];
-    for (let i in monitors) {
-        let proto = monitors[i].split(':')[0];
-        let url = monitors[i].split(':').slice(1).join(':');
-        promises.push(probe(proto, url).then(r => {
-            requestData[i] = {
-                status: r ? 'up' : 'down',
-                href: monitors[i]
-            };
-        }));
+function addMonitor(id, data) {
+    if (monitors[id]) rmMonitor(id);
+    monitors[id] = new Monitor(data);
+    lastMonitorStatus[id] = data.status;
+}
+function rmMonitor(id) {
+    delete monitors[id];
+    delete lastMonitorStatus[id];
+}
+
+let reAnnouncing = false;
+let isAnnouncing = false;
+let lastAnnounce = 0;
+async function announce() {
+    if (isAnnouncing || reAnnouncing) return;
+    lastAnnounce = new Date;
+    isAnnouncing = true;
+    const reAnnounce = () => {
+        setTimeout(() => {
+            reAnnouncing = false;
+            announce();
+        }, 3000);
+        reAnnouncing = true;
+        isAnnouncing = false;
     }
-    await Promise.all(promises);
 
-    /** @type {Fetch.Response} */
-    let resp;
+    for (let i in monitors) lastMonitorStatus[i] = monitors[i].status;
+
     try {
         resp = await axios({
             url: `${config.server}/announce`,
@@ -29,44 +45,129 @@ async function cycle() {
             responseType: 'json',
             data: JSON.stringify({
                 token: config.token,
-                data: requestData
+                data: monitors
             }),
             headers: { 'Content-Type': 'application/json' }
         });
     }
     catch (e) {
         console.log(`Something went wrong, announce request failed with error:`, e);
-        return scheduleCycle();
+        return reAnnounce();
     }
     if (resp.status != 200) {
         console.log(`Something went wrong, server responded with HTTP ${resp.status}`);
-        return scheduleCycle();
+        return reAnnounce();
     }
 
     let ans = resp.data;
     if (!ans) {
         console.log(`Something went wrong, server's answer is empty`);
-        return scheduleCycle();
+        return reAnnounce();
     }
     if (!ans.ok) {
         console.log(`Something went wrong, server responded with code "${ans.code}"`);
-        return scheduleCycle();
+        return reAnnounce();
     }
     if (Array.isArray(ans.remove)) ans.remove.forEach(i => {
         console.log(`Monitor ${i} has been removed by server command`);
-        delete monitors[i];
+        rmMonitor(i);
     });
     if (ans.new) for (let i in ans.new) {
-        console.log(`New/updated monitor ${i}, href: ${ans.new[i]}`);
-        monitors[i] = ans.new[i];
+        console.log(`New/updated monitor ${i}, href: ${ans.new[i].href}`);
+        addMonitor(i, ans.new[i]);
     };
-    //console.log('Cycle sent at ' + new Date);
-    scheduleCycle();
+
+    isAnnouncing = false;
 }
 
-function scheduleCycle() {
-    //console.log('scheduled');
-    setTimeout(() => cycle(), 1000 * 60);
+setInterval(() => {
+    let haveUpdatedMonitors = false;
+    for (let i in monitors) {
+        let m = monitors[i];
+        if (m.status != lastMonitorStatus[i]) {
+            haveUpdatedMonitors = true;
+            break;
+        }
+    }
+    if (new Date - lastAnnounce >= 60000 || haveUpdatedMonitors) announce();
+}, 1000);
+
+class Monitor {
+    #destroyed = false;
+    get destroyed() { return this.#destroyed; }
+    set destroyed(v) {
+        if (v) this.#destroyed = true;
+    }
+
+    href = 'ping:0.0.0.0';
+    get #hrefProto() {
+        return this.href.split(':')[0];
+    }
+    get #hrefURL() {
+        return this.href.split(':').slice(1).join(':');
+    }
+    status = null;
+
+    constructor(jsonData) {
+        this.id = jsonData.id;
+        this.href = jsonData.href;
+        this.status = jsonData.status;
+        this.#probe();
+    }
+
+    #isProbing = false;
+    async #probe() {
+        const end = () => {
+            this.#isProbing = false;
+            setTimeout(() => this.#probe(), 15000);
+        }
+
+        if (this.destroyed) return;
+        if (this.#isProbing) return;
+        this.#isProbing = true;
+
+        for (let i = 0; i < 3; i++) {
+            if (this.destroyed) return;
+            let up = await probe(this.#hrefProto, this.#hrefURL);
+            if (up) {
+                this.status = 'up';
+                end();
+                return true;
+            }
+            else await this.#sleep(3000);
+        }
+
+        // If we got here, none of 3 probes returned true
+        this.status = 'down';
+        end();
+        return false;
+    }
+
+    #sleep(ms) {
+        return new Promise(r => {
+            setTimeout(() => r(), ms);
+        });
+    }
+
+    toJSON() {
+        return {
+            status: this.status,
+            href: this.href
+        };
+    }
 }
 
-cycle();
+if (config.expressPort) {
+    const server = Express();
+    server.set('view engine', 'ejs');
+    if (config.expressAuth) server.use(ExpressBasicAuth({ users: config.expressAuth }));
+    server.get('/monitors', (_req, resp) => {
+        let result = [];
+        for (let i in lastMonitorStatus) result.push(`${monitors[i].status == 'up' ? '🟢' : '🔴'} ${i} (${monitors[i].href.replace(/\</g, '&lt;').replace(/\>/g, '&lt;')})`);
+        resp.render('info', {
+            monitors: monitors,
+            lastAnnounce: lastAnnounce
+        });
+    });
+    server.listen(config.expressPort);
+}
--- probe.js
+++ probe.js
@@ -1,5 +1,7 @@
 const ping = require('ping').promise;
 const axios = require('axios').default;
+const fs = require('fs');
+const ChildProcess = require('child_process');
 
 module.exports = async (proto, url) => {
     //console.log('doing ', proto, url);
@@ -25,6 +27,27 @@ module.exports = async (proto, url) => {
             catch {
                 return false;
             }
+        case 'port':
+            const regexIp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+            const regexDomain = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
+            let host = url.split(':')[0];
+            let port = url.split(':')[1];
+            if (!regexIp.test(host) && !regexDomain.test(host)) return false;
+            if (isNaN(port) || port % 1 != 0 || port < 1 || port > 65535) return false;
+
+            return await new Promise(r => {
+                ChildProcess.exec(`nmap ${host} -p ${port}`, (_, stdout) => {
+                    let found = false;
+                    stdout.split('\n').forEach(l => {
+                        if (l.startsWith(`${port}/tcp `)) {
+                            const portState = l.replace(/ {2,100}/g, ' ').split(' ')[1];
+                            r(portState == 'open');
+                            found = true;
+                        }
+                    });
+                    if (!found) r(false);
+                });
+            });
         default:
             return false;
     }
